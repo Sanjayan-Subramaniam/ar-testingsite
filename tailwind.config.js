3e48834c4fdccd9618923883c74e0fe58a821fd3
Sanjayan Subramaniam <sanjayan.subramaniam@gmail.com>
2023-03-06 13:29:55 +1100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    `/components/**/*.{vue,js,ts}`,
+    `/layouts/**/*.vue`,
+    `$pages/**/*.vue`,
+    `/composables/**/*.{js,ts}`,
+    `/plugins/**/*.{js,ts}`,
+    `/App.{js,ts,vue}`,
+    `/app.{js,ts,vue}`,
+    `/Error.{js,ts,vue}`,
+    `/error.{js,ts,vue}`,
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ardarkgreen: "#4a6b2c",
+        arprimary: "#679941",
+        arsecondary: "#7fb655",
+        argray: "#b0bac9",
+        ardarkgray: "#9c9a9a)",
+        arlightgray: "#d7dce4",
+        arblack: "#3a3636",
+        arblue: "#4192ec",
+        twitterblue: "#1DA1F2",
+        facebookblue: "#3b5998",
+        linkedinblue: "#0072b1",
+      },
+
+      fontFamily: {
+        sans: ["'Saira'"],
+      },
+
+      backgroundImage: {
+        "lionel-background": "url('img/Lionel on site.JPG')",
+        "construction-background": "url('img/Construction Background.png')",
+        "construction-background-full":
+          "url('img/Construction Background Full.png')",
+        "exhibition-background": "url('img/Exhibition Background.png')",
+        "exhibition-background-full":
+          "url('img/Exhibition Background Full.png')",
+        "disinfection-background": "url('img/Disinfection Background.png')",
+        "disinfection-background-full":
+          "url('img/Disinfection Background Full.png')",
+        "lionel-contactsection":
+          "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('img/Construction Background.png')",
+      },
+    },
+  },
+  plugins: [],
+};
