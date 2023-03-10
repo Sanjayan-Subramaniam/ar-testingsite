6da6dbb7daae87b6ecf95f2c6a61f4742dcdedc3
Sanjayan Subramaniam <sanjayan.subramaniam@gmail.com>
2023-03-10 13:41:55 +1100

    Changed picture card component to use a BG image

--- components/Picturecard.vue
+++ components/Picturecard.vue
@@ -2,18 +2,22 @@
   <div
     class="bg-white shadow-lg w-[240px] xl:w-[360px] h-[520px] lg:h-[560px] xl:h-[640px] rounded-lg"
   >
-    <img
+    <div
+      class="w-[240px] xl:w-[360px] h-[200px] xl:h-[240px] bg-cover bg-center"
+      :class="imagePath"
+    ></div>
+    <!-- <img
       :src="imagePath"
       alt=""
       class="w-[300px] xl:w-[400px] h-[200px] xl:h-[240px] object-cover"
-    />
+    /> -->
     <div class="flex flex-col p-10 gap-y-6">
       <h4
-        class="text-arprimary text-mobileCopy xl:text-desktopCopy w-[200px] xl:w-[280px]"
+        class="text-arprimary text-mobileCopy xl:text-desktopCopy w-[160px] xl:w-[280px]"
       >
         {{ title }}
       </h4>
-      <p class="text-mobileCopy xl:text-desktopCopy w-300px=">
+      <p class="text-mobileCopy xl:text-desktopCopy w-[300px]=">
         {{ copy }}
       </p>
     </div>
