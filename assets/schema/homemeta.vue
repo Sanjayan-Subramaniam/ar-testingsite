d63cc922662d2c30f13a8cb3754a0fa576d586c0
Sanjayan Subramaniam <sanjayan.subramaniam@gmail.com>
2023-03-30 21:15:12 +1100

    pushing homemeta.vue again

---
+++ assets/schema/homemeta.vue
@@ -0,0 +1,16 @@
+<Schema
+  
+/>
+<Seo
+  title="August Robotics - the world's leading autonomous commercial robotics company"
+  ,
+  description="We automate dirty, dangerous and dull tasks all over the world. "
+  keywords={'robotics, commericial robotics, autonomous robotics, construction layout, exhibition floor marking, commerical disinfection, lionel, diego '.split(
+    ','
+  )}
+  icon="/favicon.svg"
+  
+/>
+
+
+</Schema>
