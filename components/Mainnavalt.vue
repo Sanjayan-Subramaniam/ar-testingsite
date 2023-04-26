6084effffc6df7b010f0376dbae59059d5aa1ef7
Sanjayan-Subramaniam <69328849+Sanjayan-Subramaniam@users.noreply.github.com>
2023-04-26 13:30:06 +1000

    Made the news section visible on the main nav again

--- components/Mainnavalt.vue
+++ components/Mainnavalt.vue
@@ -96,7 +96,7 @@
             <NuxtLink
               @click="companyOpen === false"
               to="/news"
-              class="hidden block rounded-lg py-2 px-3 text-sm font-semibold leading-6 text-arblack hover:bg-arsecondary/50 hover:text-white z-50"
+              class="block rounded-lg py-2 px-3 text-sm font-semibold leading-6 text-arblack hover:bg-arsecondary/50 hover:text-white z-50"
               >News</NuxtLink
             >
           </div>
@@ -226,7 +226,7 @@
                   <Disclosure
                     ><NuxtLink to="/news"
                       ><DisclosureButton
-                        class="hidden block rounded-lg py-2 pl-6 pr-64 text-sm font-semibold leading-7 text-arblack/50 hover:bg-ardarkgreen/50 hover:text-white"
+                        class="block rounded-lg py-2 pl-6 pr-64 text-sm font-semibold leading-7 text-arblack/50 hover:bg-ardarkgreen/50 hover:text-white"
                         @click="mobileMenuOpen = false"
                         >News</DisclosureButton
                       ></NuxtLink
