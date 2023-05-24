3e0e092852311c9af98b9faf8163066110538981
Sanjayan-Subramaniam <69328849+Sanjayan-Subramaniam@users.noreply.github.com>
2023-05-24 10:35:14 +1000

    added a video to the Diego page

---
+++ components/Diego/Features/Videos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mx-auto flex flex-col gap-y-6">
+    <p class="text-mobileCopy lg:text-desktopCopy mx-auto">
+      Watch Diego's Autonomous UVC Disinfection in Action
+    </p>
+
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/pqImoQY6eT4"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+      class="mx-auto w-[320px] h-[180px] md:w-[640px] md:h-[360px] lg:w-[1024px] lg:h-[576px]"
+    ></iframe>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
