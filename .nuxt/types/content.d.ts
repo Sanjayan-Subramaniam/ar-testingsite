60706c0a5ebf1f0efa17b5f4abe88feccdda9345
Sanjayan-Subramaniam <69328849+Sanjayan-Subramaniam@users.noreply.github.com>
2023-08-11 16:39:19 +1000

    deleted Nuxt folder

--- .nuxt/types/content.d.ts
+++ .nuxt/types/content.d.ts
@@ -1,4 +1,4 @@
 declare module '#content/server' {
-  const serverQueryContent: typeof import('/Users/swizzleverse/Documents/August Robotics/Corporate Website/ar-website-nuxt/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
-  const parseContent: typeof import('/Users/swizzleverse/Documents/August Robotics/Corporate Website/ar-website-nuxt/node_modules/@nuxt/content/dist/runtime/server').parseContent
+  const serverQueryContent: typeof import('/Users/swizzleverse/Documents/August Robotics/Xstagingsite/ar-website-nuxt/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
+  const parseContent: typeof import('/Users/swizzleverse/Documents/August Robotics/Xstagingsite/ar-website-nuxt/node_modules/@nuxt/content/dist/runtime/server').parseContent
 }
