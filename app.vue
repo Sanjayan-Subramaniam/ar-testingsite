87c3f8be0da1a93f0d23ad4a59d8c534ae0a3da3
Sanjayan-Subramaniam <69328849+Sanjayan-Subramaniam@users.noreply.github.com>
2024-05-13 11:43:53 +1000

    added new TWIPLA tracking code

--- app.vue
+++ app.vue
@@ -84,23 +84,16 @@ useHead({
     // window._linkedin_data_partner_ids.push(_linkedin_partner_id);
     // `,
     //     },
-    //     {
-    //       // type: "vistoranalytics",
-    //       type: "text/javascript",
-    //       tagPosition: "bodyClose",
+    {
+      // type: "vistoranalytics",
+      type: "text/javascript",
+      tagPosition: "bodyClose",
 
-    //       innerHTML: `
+      innerHTML: `
 
-    //       (function(l) {
-    // if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-    // window.lintrk.q=[]}
-    // var s = document.getElementsByTagName("script")[0];
-    // var b = document.createElement("script");
-    // b.type = "text/javascript";b.async = true;
-    // b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-    // s.parentNode.insertBefore(b, s);})(window.lintrk);
-    // `,
-    //     },
+          (function(v,i,s,a,t){v[t]=v[t]||function(){(v[t].v=v[t].v||[]).push(arguments)};if(!v._visaSettings){v._visaSettings={}}v._visaSettings[a]={v:'1.0',s:a,a:'1',t:t};var b=i.getElementsByTagName('body')[0];var p=i.createElement('script');p.defer=1;p.async=1;p.src=s+'?s='+a;b.appendChild(p)})(window,document,'//app-worker.visitor-analytics.io/main.js','f7e03de4-10c8-11ef-8f4b-ea58703f3ef6','va')
+    `,
+    },
   ],
 });
 </script>
