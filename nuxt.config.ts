dc90f4da929a3e53b64a772b844e7fbf3c4fe592
Sanjayan-Subramaniam <69328849+Sanjayan-Subramaniam@users.noreply.github.com>
2024-05-13 11:30:13 +1000

    removed Netlify redirect TOML file and trying Nuxt redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // routeRules: {
-  //   "/": { redirect: "/home" },
-  // },
+  routeRules: {
+    "/": { redirect: "/home/" },
+  },
 
   modules: [
     "@pinia/nuxt",
